dc9e3b423cfd94922ee3b22793b89ac2ab6d54a3
Simon Nystrom <52610222+simstrom@users.noreply.github.com>
2021-12-01 19:15:53 +0100

    add basic CRUD Strucutre Routes for campgrounds

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const { urlencoded } = require('express');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/camp-spotter');
@@ -15,19 +17,55 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
+app.use(urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
 	res.render('home');
 });
 
+// Renders index page of all campgrounds.
 app.get('/campgrounds', async (req, res) => {
 	const campgrounds = await Campground.find({});
 	res.render('campgrounds/index', { campgrounds });
 });
 
+// Renders form for creating new campgrounds.
+app.get('/campgrounds/new', (req, res) => {
+	res.render('campgrounds/new');
+});
+
+// Saves a new campground to the db.
+app.post('/campgrounds', async (req, res) => {
+	const newCamp = new Campground(req.body.campground);
+	await newCamp.save();
+	res.redirect(`/campgrounds/${newCamp._id}`);
+});
+
+// Renders page for individual campground
 app.get('/campgrounds/:id', async (req, res) => {
 	const campground = await Campground.findById(req.params.id);
 	res.render('campgrounds/show', { campground });
+});
+
+// Renders form for for edit individual campground
+app.get('/campgrounds/:id/edit', async (req, res) => {
+	const campground = await Campground.findById(req.params.id);
+	res.render('campgrounds/edit', { campground });
+});
+
+// Handles update request to individual campground.
+app.put('/campgrounds/:id', async (req, res) => {
+	const { id } = req.params;
+	await Campground.findByIdAndUpdate(id, req.body.campground);
+	res.redirect(`/campgrounds/${id}`);
+});
+
+// Handles delete request for individual campground.
+app.delete('/campgrounds/:id', async (req, res) => {
+	// const { id } = req.params;
+	await Campground.findByIdAndDelete(req.params.id);
+	res.redirect('/campgrounds');
 });
 
 app.listen(3000, () => {
