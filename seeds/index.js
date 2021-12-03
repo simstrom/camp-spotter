c0f9734299122686816a842501ad5d2b3f465996
Simon Nystrom <52610222+simstrom@users.noreply.github.com>
2021-12-03 08:53:04 +0100

    updated campground model and improved styling on all routes

--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,14 @@ const seedDatabase = async () => {
 	// await Campground.deleteMany({});
 	for (let i = 0; i < 50; i++) {
 		const rand = Math.floor(Math.random() * 1000);
+		const price = Math.floor(Math.random() * 20) + 10;
 		const camp = new Campground({
 			location: `${locations[rand].city}, ${locations[rand].state}`,
 			title: `${sample(descriptors)} ${sample(places)}`,
+			image: 'https://source.unsplash.com/collection/483251',
+			description:
+				'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae natus ipsum, repudiandae officiis non esse nulla consectetur explicabo voluptate rerum. Repellat quod odio ducimus mollitia perferendis distinctio culpa blanditiis corrupti?',
+			price,
 		});
 		await camp.save();
 	}
