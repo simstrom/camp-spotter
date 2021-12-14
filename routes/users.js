d40c121221e19d1bd50d205921255dd0624a9bc5
Simon Nystrom <52610222+simstrom@users.noreply.github.com>
2021-12-14 19:03:44 +0100

    add register and login logic

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,48 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
 router.get('/register', (req, res) => {
 	res.render('users/register');
 });
 
-router.post('/register', async (req, res) => {
-	res.send(req.body);
+router.post(
+	'/register',
+	catchAsync(async (req, res) => {
+		try {
+			const { email, username, password } = req.body;
+			const user = new User({ email, username });
+			const registeredUser = await User.register(user, password);
+			req.flash(
+				'success',
+				`Welcome to CampSpotter, ${
+					username.charAt(0).toUpperCase() + username.slice(1)
+				}!`
+			);
+			res.redirect('/campgrounds');
+		} catch (err) {
+			req.flash('error', err.message);
+			res.redirect('/register');
+		}
+	})
+);
+
+router.get('/login', (req, res) => {
+	res.render('users/login');
 });
+
+router.post(
+	'/login',
+	passport.authenticate('local', {
+		failureFlash: true,
+		failureRedirect: '/login',
+	}),
+	(req, res) => {
+		req.flash('success', 'Welcome back!');
+		res.redirect('/campgrounds');
+	}
+);
 
 module.exports = router;
