592628837b83530313d97a2fdfae23fdd2cf17c5
Simon Nystrom <52610222+simstrom@users.noreply.github.com>
2021-12-16 21:52:31 +0100

    add improved styling for all routes

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -1,10 +1,12 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-	container: 'map', // container ID
+	container: 'details-map', // container ID
 	style: 'mapbox://styles/mapbox/light-v10', // style URL
 	center: campground.geometry.coordinates, // starting position [lng, lat]
 	zoom: 10, // starting zoom
 });
+
+map.addControl(new mapboxgl.NavigationControl());
 
 new mapboxgl.Marker({ color: '#2EC4B6' })
 	.setLngLat(campground.geometry.coordinates)
